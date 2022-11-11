eeda414aafcf238f6b2ac357d0832bd94e1d5be9
Youssef tdl <usftadlaoui@gmail.com>
2022-11-11 23:59:05 +0100

    dashbord

--- index.php
+++ index.php
@@ -21,31 +21,43 @@
 				filter: blur(5px);
 
 		}
-		nav,section{
+		nav,section,footer{
 			position : relative;
-			margin-bottom :15%;
 		}
 		.citation{
-			background: rgba( 255, 255, 255, 0.1 );
-			box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-			backdrop-filter: blur( 2.5px );
-			-webkit-backdrop-filter: blur( 2.5px );
-			border-radius: 10px;
-			border: 1px solid rgba( 255, 255, 255, 0.18 );
-			width:40%
-			
+				background: rgba( 255, 255, 255, 0.1 );
+				box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+				backdrop-filter: blur( 2.5px );
+				-webkit-backdrop-filter: blur( 2.5px );
+				border-radius: 10px;
+				border: 1px solid rgba( 255, 255, 255, 0.18 );
+				width:40%;
 		}
+        @media only screen and (max-width: 910px) {
+             .nav-b{
+				display: none;
+			 }
+                                                  }
+		@media only screen and (max-width: 600px) {
+             .text-white.citation.me-3{
+				display: none;
+			 }
+			 .citation{
+				width:60%;
+				margin-bottom :20%;
+			 }
 
+                                                  }
 
 
 		</style>
 </head>
 <body>
 	<div class="logo"></div>
-	<nav>
+	<nav style ="margin-bottom :15%;">
 		<div class="d-flex justify-content-around pt-5">
 			<div class="text-white h1"><span class="navbar-logo text-danger "><i class="bi bi-music-note-list"></i></span> <b class="">RockStars</b></div>
-					<div class="">
+					<div class="nav-b">
            						<ul class="d-flex list-unstyled ">
 									<li><a href="" class="text-decoration-none text-white h3 me-5" ><b>Home</b></a></li>
 									<li><a href="" class="text-decoration-none text-white h3 me-5"><b>Service</b></a></li>
@@ -59,15 +71,80 @@
 							</div>
 		</div>
 	</nav>
-	<section>
-		<div class="d-flex justify-content-around ">
-		<div class="text-white citation me-3">
-			<h1>Le but de l'art, le but d'une vie ne peut être que d'accroître la somme de liberté et de responsabilité qui est dans chaque homme et dans le monde.</h1>
-		</div>
-		<div class=" text-white citation"> <h1>RockStars c'est un application de gestion qui aide l'utilisateur gére en ligne de son stock des instruments de musique pour maintenir tout le travail quotidien du stock.</h1> </div>
-		</div>
-
+	<section >
+				<div class="d-flex justify-content-around ">
+						<div class="text-white citation me-3">
+									<h1>Le but de l'art, le but d'une vie ne peut être que d'accroître la somme de liberté et de responsabilité qui est dans chaque homme et dans le monde.</h1>
+						</div>
+						<div class=" text-white citation "> <h1>RockStars c'est un application de gestion qui aide l'utilisateur gére en ligne de son stock des instruments de musique pour maintenir tout le travail quotidien du stock.</h1> </div>
+				</div>
 	</section>
+	<footer class=" text-center text-white " style="background-color: rgba(0, 0, 0, 0.2);
+	margin-top: 13.5%;">
+	    	
+					<!-- Grid container -->
+					<div class="container p-4 pb-0 ">
+						<!-- Section: Social media -->
+						<section class="mb-4">
+						<!-- Facebook -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #3b5998;"
+							href="#!"
+							role="button"
+							><i class="bi bi-facebook"></i></a>
+
+						<!-- Twitter -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #55acee;"
+							href="#!"
+							role="button"
+							><i class="bi bi-twitter"></i></a>
+
+						<!-- Google -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #dd4b39;"
+							href="#!"
+							role="button"
+							><i class="bi bi-google"></i></a>
+
+						<!-- Instagram -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #ac2bac;"
+							href="#!"
+							role="button"
+							><i class="bi bi-instagram"></i></a>
+
+						<!-- Linkedin -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #0082ca;"
+							href="#!"
+							role="button"
+							><i class="bi bi-linkedin"></i></a>
+						<!-- Github -->
+						<a
+							class="btn text-white btn-floating m-1"
+							style="background-color: #333333;"
+							href="#!"
+							role="button"
+							><i class="bi bi-github"></i></a>
+						</section>
+						<!-- Section: Social media -->
+					</div>
+					<!-- Grid container -->
+
+					<!-- Copyright -->
+					<div class="text-center p-3 " >
+						© 2022 Copyright:
+						<a class="text-white" href="#">RockStars</a>
+					</div>
+					<!-- Copyright -->
+			
+</footer>
     
 </body>
 </html>
---
+++ principal.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ================== BEGIN core-css ================== -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styleee.css">
+	    <!-- ================== END core-css ================== -->
+    <title>Dashbord</title>
+</head>
+<body class="bg-danger">
+    <div class="page d-flex">
+        <div class="sidebar bg-white position-relative text-center" style= "padding :20px";>
+            <h3 class="position-relative  mt-0">RockStars</h3>
+            <ul style="padding-left: 0px;">
+                <li>
+                    <a class="active d-flex align-items-center h6 text-dark p-4" style="border-radius 6px;" href="principal.php">
+                    <i class="bi bi-graph-down me-2"></i>
+                    <span>Dashbord</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" d-flex align-items-center h6 text-dark p-4" style="border-radius 6px;" href="principal.php">
+                    <i class="bi bi-gear-fill me-2"></i>
+                    <span>Settings</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" d-flex align-items-center h6 text-dark p-4" style="border-radius 6px;" href="principal.php">
+                    <i class="bi bi-person-circle me-2"></i>
+                    <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" d-flex align-items-center h6 text-dark p-4" style="border-radius 6px;" href="principal.php">
+                    <i class="bi bi-music-note me-2"></i>
+                    <span>Les instruments</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" d-flex align-items-center h6 text-dark p-4" style="border-radius 6px;" href="principal.php">
+                    <i class="bi bi-plus-circle-dotted me-2"></i>
+                    <span>add instrument</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="content">content</div>
+    </div>
+   
+    
+</body>
+</html>
